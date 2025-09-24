38a9f3745f582fc93d59c53d46fa86e271eca57b
Divinitry <SpencerCodeAcc@gmail.com>
2025-09-24 14:33:26 -0400

    added server

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,33 +6,27 @@ const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 const NavBar = () => {
   return (
-    <SafeAreaView style={styles.safeArea} edges={['right','left','bottom']}>
       <View style={styles.container}>
         <AntDesign name="home" size={28} color="gray" />
         <AntDesign name="plus" size={28} color="gray" />
         <AntDesign name="user" size={28} color="gray" />
       </View>
-    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
+  container: {
     position: 'absolute',
     bottom: 0,
-    width: '100%',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  container: {
+    width: SCREEN_WIDTH * 0.9,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    width: SCREEN_WIDTH * 0.9,
     backgroundColor: 'black',
     borderRadius: 25,
     paddingVertical: 12,
-    marginBottom: 8,
+    marginBottom: 25,
+    alignSelf: 'center',
   },
 });
 
